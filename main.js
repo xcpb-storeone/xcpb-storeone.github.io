482b506eb42c74b295ca297cfdab31197e1848eb
Xcpb Corp Ϟ <115367614+xcpb-storeone@users.noreply.github.com>
2022-10-27 22:19:19 -0300

    Delete main.js
    Signed-off-by: Xcpb Corp Ϟ <115367614+xcpb-storeone@users.noreply.github.com>

--- main.js
+++
@@ -1,9 +0,0 @@
-let accordion=document.querySelector('.accordion-list');let items=accordion.querySelectorAll('li');let questions=accordion.querySelectorAll('.accordion-title');questions.forEach(question=>question.addEventListener('click',toggleAccordion));function toggleAccordion(){thisItem=this.parentNode;items.forEach(item=>{if(thisItem==item){thisItem.classList.toggle('open');return}item.classList.remove('open')})}
-                                                       /* 
-  ██████╗ ██╗     ██████╗ ███╗   ███╗██╗  ██╗███╗   ██╗
- ██╔═████╗██║     ██╔══██╗████╗ ████║██║  ██║████╗  ██║
- ██║██╔██║██║     ██║  ██║██╔████╔██║███████║██╔██╗ ██║
- ████╔╝██║██║     ██║  ██║██║╚██╔╝██║╚════██║██║╚██╗██║
- ╚██████╔╝███████╗██████╔╝██║ ╚═╝ ██║     ██║██║ ╚████║
-  ╚═════╝ ╚══════╝╚═════╝ ╚═╝     ╚═╝     ╚═╝╚═╝  ╚═══╝ 
-                                                      */
